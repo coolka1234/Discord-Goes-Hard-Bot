e7b999852059b0b99caa0f3c1159e41166569ca5
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-06-07 16:58:56 +0200

    on message work

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,41 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
 
-load_dotenv(os.path.curdir, 'env/.env')
+load_dotenv('./env/.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    amogus_quotes = [
+        'sus',
+        'amogus',
+    ]
+
+    if message.content == 'sussy':
+        response = random.choice(amogus_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
