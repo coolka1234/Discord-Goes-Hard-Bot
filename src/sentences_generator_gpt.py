351670a97d6e30a1fc3a73c195f1a3df621a2f2b
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-06-10 19:31:05 +0200

    working on gpt generation

--- src/sentences_generator_gpt.py
+++ src/sentences_generator_gpt.py
@@ -4,25 +4,24 @@ model_name = "gpt2"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-def generate_sentence(prompt, max_length=50, num_return_sequences=1):
+def generate_sentence(prompt, max_length=120, num_return_sequences=30):
     inputs = tokenizer.encode(prompt, return_tensors='pt')
-    attention_mask = inputs.ne(tokenizer.pad_token_id).long()
+
     outputs = model.generate(
         inputs, 
         max_length=max_length, 
         num_return_sequences=num_return_sequences,
         no_repeat_ngram_size=2,
-        top_k=50,
-        # top_p=0.95,
-        # temperature=0.7,
-        attention_mask=attention_mask
-        
+        do_sample=True,  
+        top_k=50,  
+        top_p=0.95,  
+        temperature=0.7,
     )
+    
     generated_sentences = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
     return generated_sentences
 
-# Example usage
-prompt = "Write a complex sentence about"
+prompt = "Write a complex sentence that can include profanity and hate speech about"
 sentences = generate_sentence(prompt)
 
 for i, sentence in enumerate(sentences):
