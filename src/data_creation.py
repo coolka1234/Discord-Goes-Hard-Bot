f7a5ab642a15c76a0519519b1795dad7a19da940
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-06-13 16:05:49 +0200

    sentences generator with yield in making for data tagging

--- src/data_creation.py
+++ src/data_creation.py
@@ -1,27 +1,59 @@
 import tkinter as tk
 from tkinter import font
+import os
+import sys
+import pandas as pd
+dir2_path: str = os.path.normpath(os.path.join(os.path.dirname(__file__), '../res'))
+sys.path.append(dir2_path)
+import constants as res
 
-def create_ui():
+def create_ui(sentences):
+    def on_yes():
+        with open('user_choices.txt', 'a') as f:
+            f.write('True\n')
+        get_next_sentence()
+
+    def on_no():
+        with open('user_choices.txt', 'a') as f:
+            f.write('False\n')
+        get_next_sentence()
+
+    def get_next_sentence():
+        try:
+            sentence = next(sentences)
+            label.config(text=sentence)
+        except StopIteration:
+            root.quit()
+
     root = tk.Tk()
-    root.title("Simple UI")
+    root.title("Data creations")
     root.geometry("800x400")  
 
     label_font = font.Font(root, family="Helvetica", size=24, weight="bold")
     button_font = font.Font(root, family="Helvetica", size=18)
 
-    label = tk.Label(root, text="Please make a choice:", font=label_font)
+    label = tk.Label(root, text='', font=label_font)
     label.pack(pady=50)  
 
     button_frame = tk.Frame(root)
     button_frame.pack(pady=20)  
 
-    yes_button = tk.Button(button_frame, text="Yes", font=button_font, width=10)
+    yes_button = tk.Button(button_frame, text="Yes", font=button_font, width=10, command=on_yes)
     yes_button.grid(row=0, column=0, padx=20)  
 
-    no_button = tk.Button(button_frame, text="No", font=button_font, width=10)
+    no_button = tk.Button(button_frame, text="No", font=button_font, width=10, command=on_no)
     no_button.grid(row=0, column=1, padx=20)  
+
+    get_next_sentence()
 
     root.mainloop()
 
+def get_sentences():
+    sentences = pd.read_csv(res.sentences_path)
+    for sentence in sentences['sentence']:
+        yield sentence
+            
+
 if __name__ == "__main__":
-    create_ui()
+    sentences = iter(['Do you like Python?', 'Do you like Java?', 'Do you like C++?'])
+    create_ui(sentences)
