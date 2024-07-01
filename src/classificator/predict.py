8478b49b0b63e2decd06b9652a767085a28184af
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-01 14:57:27 +0200

    translation now working!

--- src/classificator/predict.py
+++ src/classificator/predict.py
@@ -1,5 +1,8 @@
 import pickle
+import googletrans
 def predict_if_hard(sentence):
+    translator = googletrans.Translator()
+    sentence = translator.translate(sentence, dest='en').text
     clf = pickle.load(open('src/classificator/model.pkl', 'rb'))
     cv = pickle.load(open('src/classificator/cv.pkl', 'rb'))
     sentence = cv.transform([sentence]).toarray()
